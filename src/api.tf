d540332ccba3f7916dd67f6b2a1d4d6654203093
sbc-diegoromerog <diegoromerog@subocol.com>
2023-05-13 09:24:41 -0500

    SBC-001: Add GitHubActions Develop

--- src/api.tf
+++ src/api.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "api_gateway" {
   name        = "${local.company}-${local.product}-${local.environment}-api"
-  description = "Api Gateway for ${local.company}-${local.product} Application"
+  description = "ApiGateWay for ${local.company}-${local.product} Application in ${local.environment}"
   tags        = local.tags
   lifecycle {
     ignore_changes = [description]
